34b7d1231a3c862f40c58b3a8a6269f214703483
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-11-25 08:48:20 +0500

    lab 9 initialized

---
+++ Lab09.sql
@@ -0,0 +1,17 @@
+-- Lab 09 -- 
+
+-- Question 01 () 
+
+rename table teacher to teach;
+
+-- Question 02 () 
+
+alter table teach add status varchar(30);
+
+-- Question 03 () 
+
+alter table teach change column status feedback varchar(40);
+
+desc teach;
+
+
